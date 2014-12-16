b1acb77be71478004d5aedf0cd0855880827e5ce
Scott Vorthmann <scott@vorthmann.org>
2014-12-15 23:00:26 -0800

    added missing Exporter.java

---
+++ src/main/java/com/vzome/api/Exporter.java
@@ -0,0 +1,28 @@
+
+//(c) Copyright 2011, Scott Vorthmann.
+
+package com.vzome.api;
+
+import java.io.Writer;
+
+import com.vzome.core.exporters.Exporter3d;
+
+public class Exporter
+{
+    private Exporter3d delegate;
+    
+    Exporter( Exporter3d privateExp )
+    {
+        this .delegate = privateExp;
+    }
+
+    public void doExport( Document model, Writer out, int height, int width ) throws Exception
+    {
+        this .delegate .doExport( model .getViewModel(), model .getRenderedModel(), out, height, width );
+    }
+
+    public String getContentType()
+    {
+        return this .delegate .getContentType();
+    }
+}
