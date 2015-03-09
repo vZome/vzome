96e7ac1a3c1e78fec42660289aebab2f26583472
Scott Vorthmann <scott@vorthmann.org>
2015-03-08 17:46:01 -0700

    updated run script and Eclipse Launch

--- run-vzome.sh
+++ run-vzome.sh
@@ -1,7 +1,7 @@
 
 cd target/dependency
 java -Xmx3048M -Dapple.laf.useScreenMenuBar=true \
-  -cp antlr-2.7.7.jar:gluegen-rt-2.2.4.jar:jogl-all-2.2.4.jar:vecmath-1.6.0.jar:vzome-core-5.0-SNAPSHOT.jar:../vzome-desktop-5.0-SNAPSHOT.jar:j3dcore-1.6.0.jar:j3dutils-1.6.0.jar \
+  -cp antlr-2.7.7.jar:gluegen-rt-2.2.4.jar:jogl-all-2.2.4.jar:vecmath-1.6.0.jar:vzome-core-0.1.jar:../vzome-desktop-5.0-SNAPSHOT.jar:j3dcore-1.6.0.jar:j3dutils-1.6.0.jar \
   org.vorthmann.zome.ui.ApplicationUI \
   -entitlement.model.edit true \
   -entitlement.lesson.edit true \
