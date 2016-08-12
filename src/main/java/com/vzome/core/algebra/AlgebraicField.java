0410a59d591e811d22a227c4f4d483f8b817d9fe
david-hall <David.42.Hall@gmail.com>
2016-08-12 00:55:25 -0400

    Add a new AlgebraicNumber test case and rename a parameter to clarify it.
    * Update .gitignore

--- src/main/java/com/vzome/core/algebra/AlgebraicField.java
+++ src/main/java/com/vzome/core/algebra/AlgebraicField.java
@@ -117,7 +117,7 @@ public abstract class AlgebraicField
         return new AlgebraicNumber( this, brs );
     }
 
-    public AlgebraicNumber createAlgebraicNumber( int ones, int irrat, int denominator, int power )
+    public AlgebraicNumber createAlgebraicNumber( int ones, int irrat, int denominator, int scalePower )
     {
         BigRational[] factors = new BigRational[ this .getOrder() ];
         factors[ 0 ] = new BigRational( ones, denominator );
@@ -125,8 +125,8 @@ public abstract class AlgebraicField
         for ( int i = 2; i < factors.length; i++ ) {
             factors[ i ] = new BigRational( 0 );
         }
-        if ( power != 0 ) {
-            AlgebraicNumber multiplier = this .createPower( power );
+        if ( scalePower != 0 ) {
+            AlgebraicNumber multiplier = this .createPower( scalePower );
             return new AlgebraicNumber( this, factors ) .times( multiplier );
         }
         else
