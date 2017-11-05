b07d1c91259e212d363a91f105b9dae87810fa87
Scott Vorthmann <scott@vorthmann.org>
2017-11-05 09:19:20 -0800

    Initial demo of embedded Jetty subproject
    Copied HelloWorld app from Jetty docs, and got it to run with “./gradlew :server:run”.

---
+++ server/src/main/java/com/vzome/server/ControllerServer.java
@@ -0,0 +1,43 @@
+package com.vzome.server;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.AbstractHandler;
+
+public class ControllerServer extends AbstractHandler
+{
+    @Override
+    public void handle( String target,
+                        Request baseRequest,
+                        HttpServletRequest request,
+                        HttpServletResponse response ) throws IOException,
+                                                      ServletException
+    {
+        // Declare response encoding and types
+        response.setContentType("text/html; charset=utf-8");
+
+        // Declare response status code
+        response.setStatus(HttpServletResponse.SC_OK);
+
+        // Write back response
+        response.getWriter().println("<h1>Hello from vZome!</h1>");
+
+        // Inform jetty that this request has now been handled
+        baseRequest.setHandled(true);
+    }
+
+    public static void main( String[] args ) throws Exception
+    {
+        Server server = new Server(8083);
+        server.setHandler(new ControllerServer());
+
+        server.start();
+        server.join();
+    }
+}
