486cc09cef2156f01b78d95ddf9ef38575419180
Scott Vorthmann <scott@vorthmann.org>
2017-12-25 08:55:26 -0600

    Trackball is working
    Added trackball support from react-three-renderer DraggableCubes example.
    Camera state is maintained in the component, unfortunately, so this is not
    really integrated with Redux.

--- client/src/containers/modelcanvas-three.js
+++ client/src/containers/modelcanvas-three.js
@@ -1,8 +1,10 @@
 
 import React from 'react';
+import PureRenderMixin from 'react/lib/ReactComponentWithPureRenderMixin';
 import { connect } from 'react-redux'
 import React3 from 'react-three-renderer';
 import * as THREE from 'three';
+import TrackballControls from './trackball';
 
 class Strut extends React.Component {
 
@@ -15,6 +17,8 @@ class Strut extends React.Component {
     // construct these vectors here, because if we use 'new' within render,
     // React will think that things have changed when they have not.
   }
+
+  shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate;
   
   render() {
     return (
@@ -31,27 +35,101 @@ class ModelCanvasThree extends React.Component {
   constructor(props, context) {
     super(props, context);
 
-    // construct the position vector here, because if we use 'new' within render,
-    // React will think that things have changed when they have not.
-    this.cameraPosition = new THREE.Vector3(0, 0, 65);
+    this.state = {
+      cameraPosition: new THREE.Vector3(0, 0, 65),
+      cameraRotation: new THREE.Euler()
+    };
   }
 
-  render() {    
+  componentDidUpdate() {
+    const {
+      camera,
+    } = this.refs;
+    
+    if ( ! this.props.enabled ) {
+    	return;
+    }
+
+    if ( this.controls ) {
+    	return;
+    }
+
+    const controls = new TrackballControls(camera);
+
+    controls.rotateSpeed = 1.0;
+    controls.zoomSpeed = 1.2;
+    controls.panSpeed = 0.8;
+    controls.noZoom = false;
+    controls.noPan = false;
+    controls.staticMoving = true;
+    controls.dynamicDampingFactor = 0.3;
+
+    this.controls = controls;
+
+    this.controls.addEventListener('change', this._onTrackballChange);
+  }
+
+  _onTrackballChange = () => {
+    this.setState({
+      cameraPosition: this.refs.camera.position.clone(),
+      cameraRotation: this.refs.camera.rotation.clone(),
+    });
+  };
+
+  _onAnimate = () => {
+    this._onAnimateInternal();
+  };
+
+  _onAnimateInternal() {
+    const {
+      camera,
+    } = this.refs;
+
+    if ( ! this.props.enabled ) {
+    	return;
+    }
+
+    if (this.state.camera !== camera) {
+      this.setState({
+        camera,
+      });
+    }
+
+    this.controls.update();
+  }
+
+  componentWillUnmount() {
+    this.controls.removeEventListener('change', this._onTrackballChange);
+
+    this.controls.dispose();
+    delete this.controls;
+  }
+
+  render() {
+
+    const {
+      cameraPosition,
+      cameraRotation,
+    } = this.state;
+
 		return (
 			this.props.enabled?
 				<React3
-					mainCamera="camera" // this points to the perspectiveCamera below
+					mainCamera="mainCamera" // this points to the perspectiveCamera below
 					antialias
+					onAnimate={this._onAnimate}
 					width={this.props.width}
 					height={this.props.height} >
 					<scene>
 						<perspectiveCamera
-							name="camera"
+							name="mainCamera"
+							ref="camera"
 							fov={75}
 							aspect={this.props.width / this.props.height}
 							near={0.1}
 							far={1000}
-							position={this.cameraPosition}
+							position={cameraPosition}
+							rotation={cameraRotation}
 						/>
 						{
 							this.props.segments.map( segment =>
