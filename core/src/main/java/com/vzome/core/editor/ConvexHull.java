8ca03e62282faaf3a1c11ddd9958b8629278d01b
David Hall <David.42.Hall@gmail.com>
2018-08-27 14:15:10 -0400

    Add 2d and 3d convex hull commands to core

---
+++ core/src/main/java/com/vzome/core/editor/ConvexHull.java
@@ -0,0 +1,39 @@
+package com.vzome.core.editor;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import com.vzome.core.algebra.AlgebraicVector;
+import com.vzome.core.model.Connector;
+import com.vzome.core.model.Manifestation;
+import com.vzome.core.model.Panel;
+import com.vzome.core.model.RealizedModel;
+import com.vzome.core.model.Strut;
+
+public abstract class ConvexHull extends ChangeManifestations {
+    public ConvexHull(Selection selection, RealizedModel realized) {
+        super(selection, realized);
+    }
+    
+    // common code for 2d and 3d convex hulls
+    protected Set<AlgebraicVector> getSelectedVertexSet(boolean unselectAll) {
+        Set<AlgebraicVector> vertexSet = new HashSet<>();
+       
+        for (Manifestation man : mSelection) {
+            if (man instanceof Connector) {
+                vertexSet.add( man.getLocation() );
+            } else if (man instanceof Strut) {
+                vertexSet.add( man.getLocation() );
+                vertexSet.add( ((Strut) man).getEnd() );
+            } else if (man instanceof Panel) {
+                for(AlgebraicVector vertex : ((Panel) man)) {
+                    vertexSet.add( vertex );
+                }
+            }
+            if(unselectAll) {
+                unselect(man);
+            }
+        }
+        return vertexSet;
+    }
+}
--- core/src/main/java/com/vzome/core/editor/DocumentModel.java
+++ core/src/main/java/com/vzome/core/editor/DocumentModel.java
@@ -400,6 +400,12 @@ public class DocumentModel implements Snapshot .Recorder, UndoableEdit .Context
         case "JoinPoints":
             edit = new JoinPoints( this.mSelection, this.mRealizedModel, groupInSelection );
             break;
+        case ConvexHull2d.NAME:
+            edit = mEditorModel.convexHull2d();
+            break;
+        case ConvexHull3d.NAME:
+            edit = mEditorModel.convexHull3d();
+            break;
         case JoinSkewLines.NAME:
             edit = mEditorModel.joinSkewLines();
             break;
@@ -662,6 +668,12 @@ public class DocumentModel implements Snapshot .Recorder, UndoableEdit .Context
 		case "joinBallsAllPossible":
 			edit = new JoinPoints( mSelection, mRealizedModel, false, JoinPoints.JoinModeEnum.ALL_POSSIBLE );
 			break;
+        case ConvexHull2d.NAME:
+            edit = mEditorModel.convexHull2d();
+            break;
+        case ConvexHull3d.NAME:
+            edit = mEditorModel.convexHull3d();
+            break;
         case JoinSkewLines.NAME:
             edit = mEditorModel.joinSkewLines();
             break;
