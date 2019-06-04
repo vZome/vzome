186d0d6b25c5bbf99912e875ca7a3f702383e97c
David Hall <David.42.Hall@gmail.com>
2019-06-03 22:50:52 -0400

    Fix GaussJordanReduction to work with non-square matrices

--- core/src/main/java/com/vzome/core/algebra/Fields.java
+++ core/src/main/java/com/vzome/core/algebra/Fields.java
@@ -5,32 +5,29 @@ package com.vzome.core.algebra;
 
 import java.util.Arrays;
 
-public class Fields
-{
-    public interface BigRationalElement<R, T> extends RationalElement<R, T>
-    {
+public class Fields {
+    public interface BigRationalElement<R, T> extends RationalElement<R, T> {
         boolean isBig();
+
         boolean notBig();
     }
-    
-    public interface RationalElement<R, T> extends Element<T>
-    {
+
+    public interface RationalElement<R, T> extends Element<T> {
         R getNumerator();
 
         R getDenominator();
 
-        T dividedBy( T that );
+        T dividedBy(T that);
 
         double evaluate();
     }
 
-    public interface Element<T>
-    {
-        T times( T that );
+    public interface Element<T> {
+        T times(T that);
 
-        T plus( T that );
+        T plus(T that);
 
-        T minus( T that );
+        T minus(T that);
 
         T reciprocal();
 
@@ -41,122 +38,140 @@ public class Fields
         boolean isOne();
     }
 
-    
-    public static final int rows( Element<?>[][] matrix )
-    {
-    	return matrix.length;
+    public static final int rows(Object[][] matrix) {
+        return matrix.length;
     }
-    
-    public static final int columns( Element<?>[][] matrix )
-    {
-    	return matrix[ 0 ] .length;
+
+    public static final int columns(Object[][] matrix) {
+        return matrix[0].length;
     }
-    
-    public static <T extends Element<T>> void matrixMultiplication( T[][] left, T[][] right, T[][] product )
-    {
-    	if ( rows( right ) != columns( left ) )
-    		throw new IllegalArgumentException( "matrices cannot be multiplied" );
-    	if ( rows( product ) != rows( left ) )
-    		throw new IllegalArgumentException( "product matrix has wrong number of rows" );
-    	if ( columns( right ) != columns( product ) )
-    		throw new IllegalArgumentException( "product matrix has wrong number of columns" );
-    	
-    	for (int i = 0; i < rows( product ); i++) {
-			for (int j = 0; j < columns( product ); j++) {
-				T sum = null;
-				for (int j2 = 0; j2 < columns( left ); j2++) {
-					T prod = left[ i ][ j2 ] .times( right[ j2 ][ j ] );
-					if ( sum == null )
-						sum = prod;
-					else
-						sum = sum .plus( prod );
-				}
-				product[ i ][ j ] = sum;
-			}
-		}
-    }
-    
-    @SuppressWarnings("unchecked")
-    public static <T extends Element<T>> int gaussJordanReduction( T[][] m, T[][] adjoined )
-    {
-        // Array "m" is copied to a local array named matrix so that m remains unchanged.
-        // The adjoined array is modified in place and will be accessible to the caller.
-        final Object[][] matrix = new Object[ m.length ][];
-        for (int i = 0; i < m.length; i++) {
-        	matrix[i] = Arrays.copyOf( m[i], m[i].length );
-        }
-        
-        // now the real work...
-        for ( int upleft = 0; upleft < matrix.length; upleft++ ) {
-            int pivot = -1;
-            // find pivot: skip columns all zero, find highest non-zero row
-            for ( int j = upleft; j < matrix[0].length; j++ ) {
-                for ( int i = upleft; i < matrix.length; i++ ) {
-                    if ( ! ((T)matrix[ i ][ j ]) .isZero() ) {
-                        pivot = i;
-                        break;
-                    }
+
+    public static <T extends Element<T>> void matrixMultiplication(T[][] left, T[][] right, T[][] product) {
+        if (rows(right) != columns(left))
+            throw new IllegalArgumentException("matrices cannot be multiplied");
+        if (rows(product) != rows(left))
+            throw new IllegalArgumentException("product matrix has wrong number of rows");
+        if (columns(right) != columns(product))
+            throw new IllegalArgumentException("product matrix has wrong number of columns");
+
+        for (int i = 0; i < rows(product); i++) {
+            for (int j = 0; j < columns(product); j++) {
+                T sum = null;
+                for (int j2 = 0; j2 < columns(left); j2++) {
+                    T prod = left[i][j2].times(right[j2][j]);
+                    if (sum == null)
+                        sum = prod;
+                    else
+                        sum = sum.plus(prod);
                 }
-                if ( pivot >= 0 ) {
+                product[i][j] = sum;
+            }
+        }
+    }
+
+    public static <T extends Element<T>> int gaussJordanReduction(T[][] matrix) {
+        // Here, the matrix is modified in place so it will be accessible to the caller.
+        return gaussJordanReduction(matrix, matrix);
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <T extends Element<T>> int gaussJordanReduction(T[][] immutableMatrix, T[][] adjoined) {
+        // matrices "immutableMatrix" and "adjoined" must have the same number of rows,
+        // but do necessarily have the same number of columns and are not necessarily square.
+        final int nRows = rows(immutableMatrix);
+
+        // All of the work is done on a copy of immutableMatrix simply named matrix
+        // so that immutableMatrix actually remains unchanged.
+        // The "adjoined" array is modified in place and will be accessible to the caller.
+        final Object[][] matrix = copyOf(immutableMatrix);
+
+        int rank = 0;
+        // find successive pivot columns, skipping columns that contain only zeroes
+        for (int col = 0; col < columns(matrix); col++) {
+            int pivotRow = -1;
+            for (int row = rank; row < nRows; row++) {
+                T element = (T) matrix[row][col];
+                if (!element.isZero()) {
+                    pivotRow = row;
                     break;
                 }
             }
-            if ( pivot < 0 ) {
-                break; // all done, matrix is now in Reduced Row Echelon Form (ReREF)
-            }
+            if (pivotRow >= 0) {
+                // swap pivot row and current rank row if necessary
+                if (pivotRow != rank) {
+                    swap(matrix, rank, pivotRow);
+                    swap(adjoined, rank, pivotRow);
+                    pivotRow = rank;
+                }
+                
+                // scale the pivot row if necessary
+                T scalar = (T) matrix[pivotRow][col];
+                if (!scalar.isOne()) {
+                    scalar = scalar.reciprocal();
+                    scale((T[]) matrix[pivotRow], scalar);
+                    scale(adjoined[pivotRow], scalar);
+                }
 
-            // exchange pivot and top rows
-            if ( pivot != upleft ) {
-                for ( int j = upleft; j < matrix[0].length; j++ ) {
-                    T temp = (T) matrix[ upleft ][ j ];
-                    matrix[ upleft ][ j ] = matrix[ pivot ][ j ];
-                    matrix[ pivot ][ j ] = temp;
-                }
-                for ( int j = 0; j < adjoined[0].length; j++ ) {
-                	T temp = adjoined[ upleft ][ j ];
-                	adjoined[ upleft ][ j ] = adjoined[ pivot ][ j ];
-                	adjoined[ pivot ][ j ] = temp;
-                }
-            }
-
-            // normalize the top row
-            if ( ! ((T) matrix[ upleft ][ upleft ]) .isOne() ) {
-                T divisor = ((T) matrix[ upleft ][ upleft ]) .reciprocal();
-                for ( int j = upleft; j < matrix[0].length; j++ ) {
-                    matrix[ upleft ][ j ] = ((T) matrix[ upleft ][ j ]) .times( divisor );
-                }
-                for ( int j = 0; j < adjoined[0].length; j++ ) {
-                	adjoined[ upleft ][ j ] = adjoined[ upleft ][ j ] .times( divisor );
-                }
-            }
-
-            // zero out all other entries in the upleft column, 
-            // by subtracting matrix[i,upleft] * matrix[upleft,j] from matrix[i,j]
-            for ( int i = 0; i < matrix.length; i++ ) {
-                if ( i != upleft && ! ((T) matrix[ i ][ upleft ]) .isZero() ) {
-                    T factor = (T) matrix[ i ][ upleft ];
-                    matrix[ i ][ upleft ] = factor .plus( factor .negate() ); // zero it out
-                    for ( int j = upleft+1; j < matrix[0].length; j++ ) {
-                        T temp = ((T) matrix[ upleft ][ j ]) .times( factor );
-                        matrix[ i ][ j ] = ((T) matrix[ i ][ j ]) .plus( temp .negate() );
-                    }
-                    for ( int j = 0; j < adjoined[0].length; j++ ) {
-                    	T temp = adjoined[ upleft ][ j ] .times( factor );
-                    	adjoined[ i ][ j ] = adjoined[ i ][ j ] .plus( temp .negate() );
+                // use pivot operation to zero out all entries in the current column 
+                // except for the pivot row
+                for (int row = 0; row < nRows; row++) {
+                    if (row != pivotRow) {
+                        scalar = ((T) matrix[row][col]);
+                        if (!scalar.isZero()) {
+                            scalar = scalar.negate();
+                            pivot(matrix, row, scalar, pivotRow);
+                            pivot(adjoined, row, scalar, pivotRow);
+                        }
                     }
                 }
+                rank++;
             }
         }
-        // since the caller won't have access to the matrix in reduced row echelon form,
-        // we'll return its rank so the caller can determine if it is invertable
-        int rank = 0;
-        for ( int i = 0; i < matrix.length; i++ ) {
-            if(i < matrix[i].length ) {
-                if(((T) matrix[i][i]) .isOne()) {
-                    rank++;
-                }
-            }
+        // After being reduced to Row Echelon Form (ReREF),
+        // rank is the number of matrix rows having a one in one of their elements
+        return rank;   
+    }
+
+    // Elementary matrix operations used in Gauss Jordan Reduction are implemented as individual functions
+    
+    private static <T extends Element<T>> Object[][] copyOf(T[][] matrix) {
+        final int nRows = rows(matrix);
+        final int nCols = columns(matrix);
+        final Object[][] copy = new Object[nRows][];
+        for (int i = 0; i < nRows; i++) {
+            copy[i] = Arrays.copyOf(matrix[i], nCols);
         }
-        return rank;
+        return copy;
+    }
+
+    /**
+     * 
+     * @param array of elements to be swapped 
+     * @param r index of the first element to be swapped
+     * @param s index of the second element to be swapped
+     * <br/>
+     * Note that since Java implements a multi-dimensional array as an array of arrays,
+     * the {@code array} parameter can be an {@code Object[][]} in which case
+     * entire rows are swapped rather than an element at a time. 
+     * Besides being more efficient at run time, this also means 
+     * that rows of multi-dimensional arrays do not necessarily have to be the same length.
+     */
+    private static void swap(Object[] array, int r, int s) {
+        Object temp = array[r];
+        array[r] = array[s];
+        array[s] = temp;
+    }
+
+    private static <T extends Element<T>> void scale(T[] array, T scalar) {
+        for (int col = 0; col < array.length; col++) {
+            array[col] = scalar.times(array[col]);
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    private static <T extends Element<T>> void pivot(Object[][] matrix, int row, T scalar, int rank) {
+        for (int col = 0; col < columns(matrix); col++) {
+            matrix[row][col] = ((T) matrix[row][col]).plus(((T) matrix[rank][col]).times(scalar));
+        }
     }
 }
