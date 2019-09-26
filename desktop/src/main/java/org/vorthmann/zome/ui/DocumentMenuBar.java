3eb6c54da36474ffffdef05fd2be408dab137ac8
Scott Vorthmann <scott@vorthmann.org>
2019-09-25 21:52:56 -0700

    Change menu command for SelectByPlane

--- desktop/src/main/java/org/vorthmann/zome/ui/DocumentMenuBar.java
+++ desktop/src/main/java/org/vorthmann/zome/ui/DocumentMenuBar.java
@@ -241,7 +241,8 @@ public class DocumentMenuBar extends JMenuBar implements PropertyChangeListener
         submenu.add( enableIf( isEditor, createMenuItem( "All", ( "DeselectAll" ) ) ) );
         menu.add(submenu);
 
-        menu.add( enableIf( isEditor, createMenuItem( "Select by Plane", ( "SelectByPlane" ) ) ) );
+        menu .addSeparator(); // %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        menu.add( enableIf( isEditor, createMenuItem( "Select Half Space", ( "SelectByPlane" ) ) ) );
         menu.add( enableIf( isEditor, createMenuItem( "Select by Diameter", ( "SelectByDiameter" ) ) ) );
         menu.add( enableIf( isEditor, createMenuItem( "Select by Radius", ( "SelectByRadius" ) ) ) );
 
