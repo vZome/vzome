e37a39446a47b85d8bde7b19757d92edf772beb8
Scott Vorthmann <scott@vorthmann.org>
2019-11-24 10:54:32 -0800

    Remove Ionic framework
    Re-created this with create-react-app, then pulled in bundles and components,
    installed necessary packages, and hooked up the provider and the view3d
    component.

---
+++ pwa/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import View3d from './components/view3d'
+
+function App() {
+  return (
+    <div className="App">
+      <View3d/>
+    </div>
+  );
+}
+
+export default App;
