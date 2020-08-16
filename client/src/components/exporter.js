956c89ef6291518e66cfde5f155cf0700c324cf8
Scott Vorthmann <scott@vorthmann.org>
2020-08-15 20:48:39 -0700

    Using react-bootstrap; new model chooser; modal for export
    The export is still just COLLADA, but the UI is much nicer.
    
    There are now three built-in models supported, and a nice dodec icon to
    let the user choose.

--- client/src/components/exporter.js
+++ client/src/components/exporter.js
@@ -1,15 +1,50 @@
 
-import React from 'react'
+import React, { useState } from 'react'
 import { connect } from 'react-redux'
 import { exportTriggered } from '../bundles/vzomejava'
+import Modal from 'react-bootstrap/Modal'
+import Button from 'react-bootstrap/Button'
+import Dropdown from 'react-bootstrap/Dropdown'
 
-let Exporter = ({ enabled, doExport }) => {
-  return ( 
-    <button id="exporter" onClick={ () => doExport( "dae", "Exporting Collada (DAE)..." ) }
-        title="Export as Collada (DAE)"
-        style={{ cursor: enabled ? 'pointer' : 'default' }} >
-        <img alt="Export a DAE file" className="Icon" src="/app/export.svg" />
-    </button>
+const Exporter = ({ enabled, doExport }) =>
+{
+  const [show, setShow] = useState(false);
+
+  const handleCancel = () =>{
+    setShow( false )
+  }
+  const handleExport = () =>{
+    setShow( false )
+    doExport( "dae", "Exporting Collada (DAE)..." )
+  }
+  const handleShow = () => setShow(true);
+
+  return (
+    <>
+      <Button id="exporter" variant="link" onClick={handleShow}
+          style={{ cursor: enabled ? 'pointer' : 'default' }} >
+          <img alt="export .dae" className="Icon" src="/app/export.svg" />
+      </Button>
+
+      <Modal centered show={show} onHide={handleCancel}>
+        <Modal.Header closeButton>
+          <Modal.Title>Export as COLLADA (dae)</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <a href="https://en.wikipedia.org/wiki/COLLADA" target="_blank" rel="noopener noreferrer">COLLADA (COLLAborative Design Activity) </a>
+          is an interchange file format for interactive 3D applications.
+          COLLADA documents are XML files, usually identified with a .dae (digital asset exchange) filename extension.
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleCancel}>
+            Cancel
+          </Button>
+          <Button variant="primary" onClick={handleExport}>
+            Export
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
   )
 } 
 
