f6b0ca0b3ed22418b0c4f269dc77f12dcabdc1d5
Scott Vorthmann <scott@vorthmann.org>
2020-08-23 17:19:23 -0700

    Added bundles/index.js to encapsulate
    Now the main index.js just imports and calls createBundleStore, which is
    generic logic, driven by a simple object full of bundles.  Adding a new
    bundle will never require more than two touches of bundles/index.json.

--- client/src/index.js
+++ client/src/index.js
@@ -3,38 +3,20 @@ import { Provider } from 'react-redux'
 import { render } from 'react-dom'
 import * as serviceWorker from './serviceWorker';
 
-import { createStore, applyMiddleware, combineReducers } from 'redux'
 import logger from 'redux-logger'
 import thunk from 'redux-thunk'
 
 import './index.css';
 import App from './App';
-import * as jre from './bundles/jre'
-import * as files from './bundles/files'
-import * as alerts from './bundles/alerts'
-import * as vzomejava from './bundles/vzomejava'
-import * as camera from './bundles/camera'
-import * as lighting from './bundles/lighting'
-import * as progress from './bundles/progress'
+import createBundleStore from './bundles'
 
-const rootReducer = combineReducers( {
-  alerts: alerts.reducer,
-  jre: jre.reducer,
-  vzomejava: vzomejava.reducer,
-  lighting: lighting.reducer,
-  camera: camera.reducer,
-  progress: progress.reducer
-} )
-
-const store = createStore( rootReducer, applyMiddleware( logger, thunk, vzomejava.middleware, files.middleware ) );
-
-jre.init( window, store )
+const store = createBundleStore( [ logger, thunk ] );
 
 render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById('root')
+  document.getElementById( 'root' )
 )
 
 // If you want your app to work offline and load faster, you can change
