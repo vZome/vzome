c225da1c38c6e4cda2dab4a54a33693248a47646
Scott Vorthmann <scott@vorthmann.org>
2020-08-31 22:52:30 -0700

    NewCentroid constructor success
    I can successfully call the NewCentroid constructor.  I'm not doing anything
    else at the moment, just demonstrating that the whole bundle loads without
    error.
    
    The key was to grab the j4ts bundle.js from .jsweet/candies/js, and put
    it into the public folder next to the generated bundle, and then including
    script tags for both in index.html.
    
    I'd rather use ES6 modules, but it seems that the transpile won't work
    correctly with any of the module settings.
    
    Next step: set up the context for the edit!

--- client/src/bundles/jsweet.js
+++ client/src/bundles/jsweet.js
@@ -1,7 +1,12 @@
 
-import { RealVector } from '../jsweet/com/vzome/core/math/RealVector'
+// import { NewCentroid } from '../jsweet/com/vzome/core/edits/NewCentroid'
+
+// I can't use the ES6 module approach until I figure out how to use J4TS that way.
+//  For now, I can load the bundles in index.html, and access the packages through window.
+//
+// I should be able to use require() and a commonjs module, but it has the same J4TS build problem.
 
 export const init = ( window, store ) =>
 {
-  console.log( "Created a RealVector: " + new RealVector().spacedString() )
+  console.log( "Created a NewCentroid: " + new window.com.vzome.core.edits.NewCentroid(null,null) )
 }
