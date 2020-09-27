2a6d3dcd33155a470ff8e47e343d8eff300231ed
Scott Vorthmann <scott@vorthmann.org>
2020-09-27 10:32:58 -0700

    Switched to Material-UI for edit menu

--- client/src/components/editmenu.js
+++ client/src/components/editmenu.js
@@ -2,34 +2,65 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { commandTriggered } from '../bundles/mesh'
-import Dropdown from 'react-bootstrap/Dropdown'
+import IconButton from '@material-ui/core/IconButton';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import Divider from '@material-ui/core/Divider';
+
+const ITEM_HEIGHT = 48;
 
 const EditMenu = ({ visible, edits, doEdit }) =>
 {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
   const h4_8 = { groupName: "H4", renderGroupName: "H4", index: 8, edgesToRender: 15 }
   if ( visible )
     return (
-      <Dropdown>
-        <Dropdown.Toggle variant="success" id="dropdown-basic">
-          Edits
-        </Dropdown.Toggle>
-
-        <Dropdown.Menu>
-          <Dropdown.Item onClick={ e => doEdit( 'random' ) }>New Random</Dropdown.Item>
-          <Dropdown.Item onClick={ e => doEdit( 'allSelected' ) }>Select All</Dropdown.Item>
-          <Dropdown.Item onClick={ e => doEdit( 'allDeselected' ) }>Deselect All</Dropdown.Item>
-          <Dropdown.Item onClick={ e => doEdit( 'centroid' ) }>Centroid 1</Dropdown.Item>
-          <Dropdown.Item onClick={ e => doEdit( 'shortred' ) }>Short Red 0</Dropdown.Item>
-          <Dropdown.Item onClick={ e => doEdit( 'ShowPoint', { mode: 'origin' } ) }>Show Origin</Dropdown.Item>
-          <Dropdown.Item onClick={ e => doEdit( 'Delete' ) }>Delete</Dropdown.Item>
-          <Dropdown.Divider />
-          <Dropdown.Item onClick={ e => doEdit( 'Polytope4d', h4_8 ) }>120-Cell</Dropdown.Item>
-          <Dropdown.Divider />
+      <div>
+        <IconButton
+          aria-label="more"
+          aria-controls="long-menu"
+          aria-haspopup="true"
+          onClick={handleClick}
+        >
+          <MoreVertIcon />
+        </IconButton>
+        <Menu id="long-menu"
+          anchorEl={anchorEl} keepMounted
+          open={open} onClose={handleClose}
+          PaperProps={{
+            style: {
+              maxHeight: ITEM_HEIGHT * 4.5,
+              width: '20ch',
+            },
+          }}
+        >
+          <MenuItem onClick={ e => doEdit( 'random' ) }>New Random</MenuItem>
+          <MenuItem onClick={ e => doEdit( 'allSelected' ) }>Select All</MenuItem>
+          <MenuItem onClick={ e => doEdit( 'allDeselected' ) }>Deselect All</MenuItem>
+          <MenuItem onClick={ e => doEdit( 'centroid' ) }>Centroid 1</MenuItem>
+          <MenuItem onClick={ e => doEdit( 'shortred' ) }>Short Red 0</MenuItem>
+          <MenuItem onClick={ e => doEdit( 'ShowPoint', { mode: 'origin' } ) }>Show Origin</MenuItem>
+          <MenuItem onClick={ e => doEdit( 'Delete' ) }>Delete</MenuItem>
+          <Divider />
+          <MenuItem onClick={ e => doEdit( 'Polytope4d', h4_8 ) }>120-Cell</MenuItem>
+          <Divider />
           { edits.map( edit =>
-            <Dropdown.Item key={edit} onClick={ e => doEdit( edit ) }>{edit}</Dropdown.Item>
+            <MenuItem key={edit} onClick={ e => doEdit( edit ) } >
+              {edit}
+            </MenuItem>
           ) }
-        </Dropdown.Menu>
-      </Dropdown>
+        </Menu>
+      </div>
     )
   else
     return null
