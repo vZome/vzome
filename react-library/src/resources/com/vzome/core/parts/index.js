131f03bed5f88fef4ffa88e9b8f0258048f53041
Scott Vorthmann <scott@vorthmann.org>
2021-01-26 08:08:21 -0800

    Corrected missed shape export

--- react-library/src/resources/com/vzome/core/parts/index.js
+++ react-library/src/resources/com/vzome/core/parts/index.js
@@ -1,19 +1,20 @@
 
 import defaultShapes from './default/index.js'
-import lifelike from './lifelike/index.js'
-import tiny from './tiny/index.js'
 import dodecs from './dodecs/index.js'
+import lifelike from './lifelike/index.js'
 import octahedral from './octahedral/index.js'
 import octahedralFast from './octahedralFast/index.js'
 import octahedralRealistic from './octahedralRealistic/index.js'
+import tiny from './tiny/index.js'
 
 const shapes = {
   'default': defaultShapes,
-  lifelike,
   dodecs,
+  lifelike,
   octahedral,
   octahedralFast,
   octahedralRealistic,
+  tiny,
 }
 
 export default shapes
