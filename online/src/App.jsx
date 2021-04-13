b6734141f5dcefcda1866df13cb84f9d2cbccb59
Scott Vorthmann <scott@vorthmann.org>
2021-04-12 23:17:58 -0700

    Finally found a CSS approach for the debugger
    Now the debugger and the canvas both fill the vertical space, and only
    the TreeView gets a scrollbar when needed.
    
    See the comment in debugger.jsx for where I found the solution.

--- online/src/App.jsx
+++ online/src/App.jsx
@@ -19,32 +19,18 @@ const debug = queryParams.get( 'debug' ) === 'true'
 
 const store = createBundleStore( profile, [ thunk ] );
 
-/* CSS GRID DEBUGGING TIP:
-  The DesignEditor (or the r3f Canvas inside it) will lock in an absolute height
-  on initial render.  This means that adjusting CSS layout attributes
-  on the Debugger component within the browser's debug tools may not
-  have the expected result.  For tweaking the Debugger layout, try
-  setting the 2nd Grid item to height:700px, so it won't flex initially.
-*/
-const useStyles = makeStyles((theme) => ({
-  debugDebuggerGrid: {
-    height: '750px',  // uncomment this to debug the Debugger grid layout; see above
-  },
-}))
-
 const App = () =>
 {
-  const classes = useStyles();
   return (
     <Provider store={store}>
         <VZomeAppBar/>
         { debug ?
           <div style={{ flex: '1', height: '100%' }}>
-            <Grid id='debug-main' container spacing={0}>
+            <Grid id='debug-main' container spacing={0} style={{ height: '100%' }}>
               <Grid id='debugger-grid-item' item xs={4}>
                 <Debugger/>
               </Grid>
-              <Grid item xs={8} className={classes.debugDebuggerGrid}>
+              <Grid item xs={8}>
                 <DesignEditor/>
               </Grid>
             </Grid>
