62aa424e57158f64b95e97af5d14603b3c4adc85
Scott Vorthmann <scott@vorthmann.org>
2021-07-28 12:51:11 -0700

    Modified PlasticPhiField to fit icosahedral symmetry
    I made two switches in the order of terms, so they become:
    
      (1,phi) X (1,rho,rho^2)

--- core/src/main/java/com/vzome/core/algebra/PlasticPhiField.java
+++ core/src/main/java/com/vzome/core/algebra/PlasticPhiField.java
@@ -1,10 +1,10 @@
 package com.vzome.core.algebra;
 
-public class PlasticPhiField extends ParameterizedField {
+public class PlasticPhiField extends ParameterizedField
+{
         public static final String FIELD_NAME = "plasticPhi";
         
         /**
-         * 
          * @return the coefficients of a PlasticPhiField. 
          * This can be used to determine when two fields have compatible coefficients 
          * without having to generate an instance of the class. 
@@ -15,10 +15,10 @@ public class PlasticPhiField extends ParameterizedField {
             final double phi = (1.0 + Math.sqrt(5)) / 2.0;
             return new double[] {
                     1.0d,
-                    plasticNumber,
                     phi,
-                    plasticNumber * plasticNumber,
+                    plasticNumber,
                     plasticNumber * phi,
+                    plasticNumber * plasticNumber,
                     plasticNumber * plasticNumber * phi
             };
         }
@@ -65,14 +65,14 @@ public class PlasticPhiField extends ParameterizedField {
           1  |  1  |  φ  
           φ  |  φ  | φ+1 
 
-          *  |  1   |  p   |   φ    |  p^2  |   pφ   |  p^2φ
-        -----+------+------+--------+-------+--------+-------------
-          1  |  1   |  p   |   φ    |  p^2  |   pφ   |  p^2φ
-          p  |  p   | p^2  |  pφ    |  1+p  |  p^2φ  |  φ+pφ 
-          φ  |  φ   | pφ   |  1+φ   | p^2φ  |  p+pφ  | p^2+p^2φ
-         p^2 | p^2  | 1+p  |  p^2φ  | p+p^2 |  φ+pφ  | pφ+p^2φ
-         pφ  | pφ   | p^2φ |  p+pφ  | φ+pφ  |p^2+p^2φ| 1+p+φ+pφ
-        p^2φ | p^2φ | φ+pφ |p^2+p^2φ|pφ+p^2φ|1+p+φ+pφ|p+p^2+pφ+p^2φ
+          *  |  1   |   φ    |  p   |   pφ   |  p^2  |  p^2φ
+        -----+------+--------+------+--------+-------+-------------
+          1  |  1   |   φ    |  p   |   pφ   |  p^2  |  p^2φ
+          p  |  p   |  pφ    | p^2  |  p^2φ  |  1+p  |  φ+pφ 
+          φ  |  φ   |  1+φ   | pφ   |  p+pφ  | p^2φ  | p^2+p^2φ
+         p^2 | p^2  |  p^2φ  | 1+p  |  φ+pφ  | p+p^2 | pφ+p^2φ
+         pφ  | pφ   |  p+pφ  | p^2φ |p^2+p^2φ| φ+pφ  | 1+p+φ+pφ
+        p^2φ | p^2φ |p^2+p^2φ| φ+pφ |1+p+φ+pφ|pφ+p^2φ|p+p^2+pφ+p^2φ
 
          */
         @Override
@@ -80,51 +80,51 @@ public class PlasticPhiField extends ParameterizedField {
             short[][][] tensor = {
                 { // 1
                     {1, 0, 0, 0, 0, 0,},
-                    {0, 0, 0, 1, 0, 0,},
-                    {0, 0, 1, 0, 0, 0,},
                     {0, 1, 0, 0, 0, 0,},
-                    {0, 0, 0, 0, 0, 1,},
                     {0, 0, 0, 0, 1, 0,},
+                    {0, 0, 0, 0, 0, 1,},
+                    {0, 0, 1, 0, 0, 0,},
+                    {0, 0, 0, 1, 0, 0,},
+                },
+                { // φ
+                    {0, 1, 0, 0, 0, 0,},
+                    {1, 1, 0, 0, 0, 0,},
+                    {0, 0, 0, 0, 0, 1,},
+                    {0, 0, 0, 0, 1, 1,},
+                    {0, 0, 0, 1, 0, 0,},
+                    {0, 0, 1, 1, 0, 0,},
                 },
                 { // p
-                    {0, 1, 0, 0, 0, 0,},
-                    {1, 0, 0, 1, 0, 0,},
-                    {0, 0, 0, 0, 1, 0,},
-                    {0, 1, 0, 1, 0, 0,},
-                    {0, 0, 1, 0, 0, 1,},
-                    {0, 0, 0, 0, 1, 1,},
-                },
-                {   // φ
                     {0, 0, 1, 0, 0, 0,},
-                    {0, 0, 0, 0, 0, 1,},
-                    {1, 0, 1, 0, 0, 0,},
-                    {0, 0, 0, 0, 1, 0,},
-                    {0, 0, 0, 1, 0, 1,},
-                    {0, 1, 0, 0, 1, 0,},
-                },
-                { // p^2
                     {0, 0, 0, 1, 0, 0,},
-                    {0, 1, 0, 0, 0, 0,},
-                    {0, 0, 0, 0, 0, 1,},
-                    {1, 0, 0, 1, 0, 0,},
-                    {0, 0, 0, 0, 1, 0,},
-                    {0, 0, 1, 0, 0, 1,},
+                    {1, 0, 0, 0, 1, 0,},
+                    {0, 1, 0, 0, 0, 1,},
+                    {0, 0, 1, 0, 1, 0,},
+                    {0, 0, 0, 1, 0, 1,},
                 },
                 {   // pφ
+                    {0, 0, 0, 1, 0, 0,},
+                    {0, 0, 1, 1, 0, 0,},
+                    {0, 1, 0, 0, 0, 1,},
+                    {1, 1, 0, 0, 1, 1,},
+                    {0, 0, 0, 1, 0, 1,},
+                    {0, 0, 1, 1, 1, 1,},
+                },
+                { // p^2
                     {0, 0, 0, 0, 1, 0,},
-                    {0, 0, 1, 0, 0, 1,},
-                    {0, 1, 0, 0, 1, 0,},
-                    {0, 0, 0, 0, 1, 1,},
-                    {1, 0, 1, 1, 0, 1,},
-                    {0, 1, 0, 1, 1, 1,},
+                    {0, 0, 0, 0, 0, 1,},
+                    {0, 0, 1, 0, 0, 0,},
+                    {0, 0, 0, 1, 0, 0,},
+                    {1, 0, 0, 0, 1, 0,},
+                    {0, 1, 0, 0, 0, 1,},
                 },
                 {   // p^2φ
                     {0, 0, 0, 0, 0, 1,},
-                    {0, 0, 0, 0, 1, 0,},
-                    {0, 0, 0, 1, 0, 1,},
-                    {0, 0, 1, 0, 0, 1,},
-                    {0, 1, 0, 0, 1, 0,},
-                    {1, 0, 1, 1, 0, 1,},
+                    {0, 0, 0, 0, 1, 1,},
+                    {0, 0, 0, 1, 0, 0,},
+                    {0, 0, 1, 1, 0, 0,},
+                    {0, 1, 0, 0, 0, 1,},
+                    {1, 1, 0, 0, 1, 1,},
                 },
             };
 
@@ -138,17 +138,17 @@ public class PlasticPhiField extends ParameterizedField {
             // The capital PHI is too similar to lower case phi so I'm going to use capital RHO. It lools like a P.
             final String upperRho = "\u03A1";
             final String lowerPhi = "\u03C6";
-            irrationalLabels[1] = new String[]{upperRho, "P"};                       // plasticNumber
-            irrationalLabels[2] = new String[]{lowerPhi, "phi"};                     // phi
-            irrationalLabels[3] = new String[]{upperRho+"\u00B2", "P^2"};            // plasticNumber^2
-            irrationalLabels[4] = new String[]{upperRho+"\u03C6", "Pphi"};           // plasticNumber * phi
+            irrationalLabels[1] = new String[]{lowerPhi, "phi"};                     // phi
+            irrationalLabels[2] = new String[]{upperRho, "P"};                       // plasticNumber
+            irrationalLabels[3] = new String[]{upperRho+"\u03C6", "Pphi"};           // plasticNumber * phi
+            irrationalLabels[4] = new String[]{upperRho+"\u00B2", "P^2"};            // plasticNumber^2
             irrationalLabels[5] = new String[]{upperRho+"\u00B2\u03C6", "P^2phi"};   // plasticNumber^2 * phi
         }
 
         @Override
         public AlgebraicNumber getGoldenRatio()
         {
-            return getUnitTerm(2);
+            return getUnitTerm(1);
         }
         
         @Override
@@ -156,9 +156,9 @@ public class PlasticPhiField extends ParameterizedField {
             if (terms.length == 2) {
                 terms = new int[] {
                         terms[0],           // units
-                        0,   // plastic
                         terms[1],           // phis
                         0,   // zero fill the rest...
+                        0,
                         0,
                         0
                 };
