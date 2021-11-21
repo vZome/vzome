c2707f955740a8a9b2e53fa1c15b3ae12a0d7f21
Scott Vorthmann <scott@vorthmann.org>
2021-11-20 17:54:38 -0800

    Adding PWA support
    I have used Workbox to implement a basic service worker with caching.
    I can install the app from either Chrome or Brave (from production),
    and it even works offline, except it cannot find the model files.
    
    In Safari on iPhone, it seems to ignore the icons from the manifest, but
    perhaps it needs a size I haven't provided.
    
    Note that only the production build has been modified to build the service
    worker bundle, which cannot be in ESM format.

---
+++ online/public/icons/README.md
@@ -0,0 +1,3 @@
+# Maskable Icons
+
+These were created using [this tool](https://maskable.app/editor), after reading [this page](https://web.dev/maskable-icon/?utm_source=devtools).
--- online/scripts/prod-build.mjs
+++ online/scripts/prod-build.mjs
@@ -2,3 +2,14 @@ import esbuild from 'esbuild';
 import { esbuildConfig } from './esbuild-config.mjs';
 
 esbuild.buildSync( { ...esbuildConfig, minify: true, sourcemap: false, outdir: 'dist/modules' } )
+
+esbuild.buildSync( {
+  entryPoints: {
+    'service-worker': 'src/app/service-worker.js',
+  },
+  bundle: true,
+  splitting: false,
+  minify: true,
+  sourcemap: false,
+  outdir: 'dist/app'
+} );
