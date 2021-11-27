8d8354878d0a257692eae7a09d953dc8c25bf45e
Scott Vorthmann <scott@vorthmann.org>
2021-11-26 21:21:27 -0800

    Material-UI is working again in the custom element
    MUI adds styles at runtime via JS, and by default they get added to the
    HTML head.  I found a solution that adds them under the shadow root:
    
    https://stackoverflow.com/questions/51832583/react-components-material-ui-theme-not-scoped-locally-to-shadow-dom

--- online/src/wc/vzome-viewer.js
+++ online/src/wc/vzome-viewer.js
@@ -3,55 +3,67 @@ import "regenerator-runtime/runtime";
 
 import React from "react";
 import ReactDOM from "react-dom";
-// @ts-ignore // no types available
+import { StylesProvider, jssPreset } from '@material-ui/styles';
+import { create } from 'jss';
+
 import * as vZome from "./urlviewer";
 import { vZomeViewerCSS } from "./vzome-viewer.css";
 
 export class VZomeViewer extends HTMLElement {
-  #root: ShadowRoot;
-  #container: HTMLElement;
+  #root;
+  #stylesMount;
+  #container;
   constructor() {
     super();
     this.#root = this.attachShadow({ mode: "open" });
 
-    this.#root.appendChild(document.createElement("style")).textContent =
-      vZomeViewerCSS;
-    this.#container = this.#root.appendChild(document.createElement("div"));
+    this.#root.appendChild(document.createElement("style")).textContent = vZomeViewerCSS;
+    this.#stylesMount = document.createElement("div");
+    this.#container = this.#root.appendChild( this.#stylesMount );
   }
 
-  connectedCallback(): void {
+  connectedCallback() {
     this.#render();
   }
 
-  #reactElement: React.ComponentElement<vZome.UrlViewer, any> | null = null;
-  get reactElement(): React.ComponentElement<vZome.UrlViewer, any> | null {
+  #reactElement = null;
+  get reactElement() {
     return this.#reactElement;
   }
 
-  #render(): void {
+  #render() {
     if (this.src === null || this.src === "") {
       ReactDOM.unmountComponentAtNode(this.#container);
       this.#reactElement = null;
       return;
     }
 
-    // TODO: Scale canvas by `window.devicePixelRatio`.
     // TODO: Can we handle canvas resizing using `ResizeObserver` without modifying `vZome` or recreating the element constantly?
-    this.#reactElement = React.createElement(vZome.UrlViewer, {
+    const viewerElement = React.createElement(vZome.UrlViewer, {
       url: this.src,
     });
-    ReactDOM.render(this.#reactElement, this.#container);
+
+    // We need JSS to inject styles on our shadow root, not on the document head.
+    // I found this solution here:
+    //   https://stackoverflow.com/questions/51832583/react-components-material-ui-theme-not-scoped-locally-to-shadow-dom
+    const jss = create({
+        ...jssPreset(),
+        insertionPoint: this.#container
+    });
+    this.#reactElement = React.createElement(StylesProvider, { jss: jss }, [ viewerElement ] );
+
+    ReactDOM.render(this.#reactElement, this.#stylesMount);
   }
 
-  static get observedAttributes(): string[] {
+  static get observedAttributes() {
     return ["src"];
   }
 
   attributeChangedCallback(
-    attributeName: string,
-    _oldValue: string,
-    _newValue: string
-  ): void {
+    attributeName,
+    _oldValue,
+    _newValue
+  ) {
     switch (attributeName) {
       case "src":
         this.#render();
@@ -59,7 +71,7 @@ export class VZomeViewer extends HTMLElement {
   }
 
   // Reflect the attribute in a JS property.
-  set src(newSrc: string | null) {
+  set src(newSrc) {
     if (newSrc === null) {
       this.removeAttribute("src");
     } else {
@@ -67,7 +79,7 @@ export class VZomeViewer extends HTMLElement {
     }
   }
 
-  get src(): string | null {
+  get src() {
     return this.getAttribute("src");
   }
 }
