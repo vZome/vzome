44dc1cf607f68b450c2452152f6d38d1fe2ad823
Scott Vorthmann <scott@vorthmann.org>
2022-09-02 09:10:34 -0700

    Updated to JSweet 3.2.0-SNAPSHOT with issue 730 fixed
    This requires local builds of jsweet (develop branch), jsweet-maven-plugin, j4ts (update-jsweet), and jsweet-gradle-plugin
    (update-jsweet).

--- cicd/jsweet-legacy-code.bash
+++ cicd/jsweet-legacy-code.bash
@@ -17,17 +17,28 @@ banner 'Transpiling Java sources with JSweet'
 
 rm -rf online/node_modules
 
-./gradlew --continue -p online jsweet -x compileJava &> jsweet-errors.txt    # ignore the exit code, it always fails
+./gradlew --continue -p online jsweetClean jsweet -x compileJava &> jsweet-errors.txt    # ignore the exit code, it always fails
 cat jsweet-errors.txt
 
-grep -q 'transpilation failed with 35 error(s) and 0 warning(s)' jsweet-errors.txt \
+grep -q 'transpilation failed with 32 error(s) and 0 warning(s)' jsweet-errors.txt \
   && banner 'JSweet transpile found the expected errors' \
   || { banner 'UNEXPECTED CHANGE IN JSWEET ERRORS'; exit 1; }
 
-banner 'Patching up the bundle as an ES6 module'
+banner 'Patching up the j4ts bundle as an ES6 module'
 
 OUTJS='online/src/worker/legacy/transpiled-java.js'
-echo 'import { java, javaemul } from "./candies/j4ts-2.0.0/bundle.js"' > $OUTJS
+echo 'import { java, javaemul } from "./candies/j4ts-2.1.0-SNAPSHOT/bundle.js"' > $OUTJS
+
+cat 'online/jsweetOut/candies/j4ts-2.1.0-SNAPSHOT/bundle.js' | \
+  sed \
+    -e 's/^var java;/export var java;/' \
+    -e 's/^var javaemul;/export var javaemul;/' \
+  > 'online/src/worker/legacy/candies/j4ts-2.1.0-SNAPSHOT/bundle.js'
+
+banner 'Patching up the vZome bundle as an ES6 module'
+
+OUTJS='online/src/worker/legacy/transpiled-java.js'
+echo 'import { java, javaemul } from "./candies/j4ts-2.1.0-SNAPSHOT/bundle.js"' > $OUTJS
 
 cat 'online/jsweetOut/js/bundle.js' | \
   sed \
