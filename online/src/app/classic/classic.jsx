92a4c68bca785c91e7ea79e64d40563107d7872d
Scott Vorthmann <scott@vorthmann.org>
2022-09-18 16:39:02 -0700

    Added more controls for OrbitPanel
    Note that the "single" checkbox state is not correct... it never shows checked.

--- online/src/app/classic/classic.jsx
+++ online/src/app/classic/classic.jsx
@@ -1,83 +1,11 @@
 
-import React, { useState } from 'react';
+import React from 'react';
 import Grid from '@material-ui/core/Grid'
-import IconButton from '@material-ui/core/IconButton'
-import SettingsIcon from '@material-ui/icons/Settings'
-import Menu from '@material-ui/core/Menu';
-import MenuItem from '@material-ui/core/MenuItem';
+import Button from '@material-ui/core/Button';
 
 import { DesignViewer } from '../../ui/viewer/index.jsx'
-import { useNewDesign, useControllerProperty, useControllerAction } from './controller-hooks.js';
-
-export const OrbitDot = ( { controllerPath, orbit, selectedOrbitNames } ) =>
-{
-  const selected = selectedOrbitNames && selectedOrbitNames .indexOf( orbit ) >= 0;
-  const orbitDetails = useControllerProperty( controllerPath, `orbitDot.${orbit}` ) || "";
-  const [ colorHex, x, y ] = orbitDetails.split( '/' );
-  let color;
-  if ( colorHex ) {
-    color = colorHex .substring( 2 ); // remove "0x"
-    if ( color .length === 2 )
-      color = `#0000${color}`
-    else if ( color .length === 4 )
-      color = `#00${color}`
-    else
-      color = `#${color}`
-  }
-  const r = 0.08;
-  return color && ( <>
-    <circle key={orbit} cx={x} cy={1-y} r={r} fill={color} />
-    { selected &&
-      <circle key={orbit+'DOT'} cx={x} cy={1-y} r={0.37*r} fill="black" />
-    }
-  </> )
-}
-
-export const OrbitPanel = ( { controllerPath, handleClick } ) =>
-{
-  const rZomeOrbits = useControllerAction( 'strutBuilder/symmetry', 'rZomeOrbits' );
-  const predefOrbits = useControllerAction( 'strutBuilder/symmetry', 'predefinedOrbits' );
-  const allOrbits = useControllerAction( 'strutBuilder/symmetry', 'setAllDirections' );
-  const orbitNames = useControllerProperty( controllerPath, 'allOrbits', 'orbits', true );
-  const selectedOrbitNames = useControllerProperty( controllerPath, 'orbits', 'orbits', true );
-  const [ anchorEl, setAnchorEl ] = useState( null );
-
-  const revealSettings = evt => setAnchorEl( evt.currentTarget );
-  const hideSettingsAnd = action => () => {
-    setAnchorEl( null );
-    action();
-  }
-
-  return (
-    <>
-      <svg viewBox="-0.2 -0.2 1.4 1.4" stroke="black" strokeWidth={0.007} onClick={handleClick}>
-        <g>
-          {/* TODO: reversed triangle per the controller */}
-          <polygon fill="none" points={`0,1 1,1 0,0`}/>  { /* all dot X & Y values are in [0..1] */ }
-          { orbitNames && orbitNames.map && orbitNames.map( orbit =>
-            <OrbitDot { ...{ controllerPath, orbit, selectedOrbitNames } }/>
-          ) }
-        </g>
-      </svg>
-      <IconButton color="inherit" aria-label="settings"
-          style={ { position: 'absolute', top: '5px', right: '5px' } }
-          onClick={revealSettings} >
-        <SettingsIcon fontSize='medium'/>
-      </IconButton>
-      <Menu
-        id="orbit-settings-menu"
-        anchorEl={anchorEl}
-        keepMounted
-        open={Boolean(anchorEl)}
-        onClose={ () => setAnchorEl( null ) }
-      >
-        <MenuItem onClick={hideSettingsAnd(rZomeOrbits)}>real Zome</MenuItem>
-        <MenuItem onClick={hideSettingsAnd(predefOrbits)}>predefined</MenuItem>
-        <MenuItem onClick={hideSettingsAnd(allOrbits)}>all</MenuItem>
-      </Menu>
-    </>
-  )
-}
+import { useNewDesign } from './controller-hooks.js';
+import { OrbitPanel } from './orbit-panel.jsx';
 
 export const ClassicEditor = () =>
 {
@@ -94,10 +22,10 @@ export const ClassicEditor = () =>
           <DesignViewer config={ { useSpinner: true } } />
         </Grid>
         <Grid id='editor-drawer' item xs={rightColumns} style={{ position: 'relative' }}>
-          <OrbitPanel controllerPath='strutBuilder/symmetry/buildOrbits'/>
-          {/* <Button variant="contained" color="primary" onClick={makeHyperdo} >
+          <OrbitPanel symmController='strutBuilder/symmetry' orbitSet='buildOrbits' />
+          <Button variant="contained" color="primary" >
             Hyperdo
-          </Button> */}
+          </Button>
         </Grid>
       </Grid>
     </div>
---
+++ online/src/app/classic/orbit-panel.jsx
@@ -0,0 +1,101 @@
+
+import React, { useState } from 'react';
+import IconButton from '@material-ui/core/IconButton'
+import SettingsIcon from '@material-ui/icons/Settings'
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import Button from '@material-ui/core/Button';
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+
+import { useControllerProperty, useControllerAction } from './controller-hooks.js';
+
+export const OrbitDot = ( { controllerPath, orbit, selectedOrbitNames, relativeHeight } ) =>
+{
+  const selected = selectedOrbitNames && selectedOrbitNames .indexOf( orbit ) >= 0;
+  const orbitDetails = useControllerProperty( controllerPath, `orbitDot.${orbit}` ) || "";
+  const [ colorHex, x, y ] = orbitDetails.split( '/' );
+  let color;
+  if ( colorHex ) {
+    color = colorHex .substring( 2 ); // remove "0x"
+    if ( color .length === 2 )
+      color = `#0000${color}`
+    else if ( color .length === 4 )
+      color = `#00${color}`
+    else
+      color = `#${color}`
+  }
+  const r = 0.05;
+  const relY = relativeHeight * ( 1-y );
+  return color && ( <>
+    <circle key={orbit} cx={x} cy={relY} r={r} fill={color} />
+    { selected &&
+      <circle key={orbit+'DOT'} cx={x} cy={relY} r={0.37*r} fill="black" />
+    }
+  </> )
+}
+
+export const OrbitPanel = ( { symmController, orbitSet } ) =>
+{
+  const symmetryAction = useControllerAction( symmController );
+  const controllerPath = symmController + '/' + orbitSet;
+  const orbitSetAction = useControllerAction( controllerPath );
+  const orbitNames = useControllerProperty( controllerPath, 'allOrbits', 'orbits', true );
+  const selectedOrbitNames = useControllerProperty( controllerPath, 'orbits', 'orbits', true );
+  const oneAtATime = useControllerProperty( controllerPath, 'oneAtATime', 'orbits', true );
+  const [ anchorEl, setAnchorEl ] = useState( null );
+
+  const revealSettings = evt => setAnchorEl( evt.currentTarget );
+  const hideSettingsAnd = action => () => {
+    setAnchorEl( null );
+    symmetryAction( action );
+  }
+
+  const marginedStyle = { margin: '8px' }
+  const relativeHeight = 0.6;
+  const triangleCorners = `0,${relativeHeight} 1,${relativeHeight} 0,0`
+  const viewBox = `-0.1 -0.1 1.2 ${relativeHeight + 0.2 }`;
+
+  return (
+    <div style={{ margin: '8px' }}>
+      <div>
+        <FormGroup row>
+          <Button variant="outlined" style={marginedStyle} onClick={ () => orbitSetAction( 'setNoDirections' ) } >None</Button>
+          <Button variant="outlined" style={marginedStyle} onClick={ () => orbitSetAction( 'setAllDirections' ) } >All</Button>
+          <FormControlLabel style={marginedStyle}
+            control={<Checkbox checked={oneAtATime} onChange={ () => orbitSetAction( 'oneAtATime' ) } />}
+            label="Single"
+          />
+        </FormGroup>
+      </div>
+      <div style={{ position: 'relative' }}>
+        <svg viewBox={viewBox} stroke="black" strokeWidth={0.005}>
+          <g>
+            {/* TODO: reversed triangle per the controller */}
+            <polygon fill="none" points={triangleCorners}/>  { /* all dot X & Y values are in [0..1] */ }
+            { orbitNames && orbitNames.map && orbitNames.map( orbit =>
+              <OrbitDot { ...{ controllerPath, orbit, selectedOrbitNames, relativeHeight } }/>
+            ) }
+          </g>
+        </svg>
+        <IconButton color="inherit" aria-label="settings"
+            style={ { position: 'absolute', top: '8px', right: '8px' } }
+            onClick={revealSettings} >
+          <SettingsIcon fontSize='medium'/>
+        </IconButton>
+        <Menu
+          id="orbit-settings-menu"
+          anchorEl={anchorEl}
+          keepMounted
+          open={Boolean(anchorEl)}
+          onClose={ () => setAnchorEl( null ) }
+        >
+          <MenuItem onClick={ hideSettingsAnd( 'rZomeOrbits' ) }>real Zome</MenuItem>
+          <MenuItem onClick={ hideSettingsAnd( 'predefinedOrbits' ) }>predefined</MenuItem>
+          <MenuItem onClick={ hideSettingsAnd( 'setAllDirections' ) }>all</MenuItem>
+        </Menu>
+      </div>
+    </div>
+  )
+}
