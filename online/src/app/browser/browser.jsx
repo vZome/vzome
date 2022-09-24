26420b027819c71d60789423e3d5bad2268913de
Scott Vorthmann <scott@vorthmann.org>
2022-09-24 16:58:25 -0700

    Improvements to the GitHub browser app
    I've formatted the list entries to highlight file names better, and added
    a button to copy the URL to the clipboard.
    
    I also improved the initial layout for the classic factory bar, so that it
    does not generate a scrollbar for the page before the icons are added.

--- online/src/app/browser/browser.jsx
+++ online/src/app/browser/browser.jsx
@@ -10,30 +10,56 @@ import Grid from '@material-ui/core/Grid'
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import IconButton from '@material-ui/core/IconButton';
+import CodeIcon from '@material-ui/icons/Code';
+import LinkIcon from '@material-ui/icons/Link';
 
 const queryParams = new URLSearchParams( window.location.search );
 const githubUser = queryParams.get( 'user' ) || "vorth";
 
 const DesignList = ( { setUrl } ) =>
 {
-  const [ designs, getDesignUrl ] = useGitHubShares( githubUser );
+  const designs = useGitHubShares( githubUser );
   const [ selectedIndex, setSelectedIndex ] = React.useState( 0 );
 
-  const handleListItemClick = (event, index) => {
-    setSelectedIndex(index);
-    setUrl( getDesignUrl( index ) );
+  const handleListItemClick = (url, index) =>
+  {
+    setSelectedIndex( index );
+    setUrl( url );
   };
+
+  const copyUrl = url =>
+  {
+    navigator.clipboard.writeText( url ) .then( () => {
+      console.log( `URL copied to the clipboard: ${url}` );
+    }, () => {
+      console.log( `URL copy FAILED: ${url}` );
+    });
+  }
 
   return (
     <div style={{ width: '100%', height: '100%', position: 'relative' }}>
-      <List component="nav" aria-label="vzome designs" style={{ overflow: 'auto', position: 'absolute', top: 0, bottom: 0, left: 0, right: 0 }} >
-      { designs.map( ( path, i ) => (
+      <List dense component="nav" aria-label="vzome designs" style={{ overflow: 'auto', position: 'absolute', top: 0, bottom: 0, left: 0, right: 0 }} >
+      { designs.map( ( { title, details, url }, i ) => (
         <ListItem key={i}
           button
           selected={selectedIndex === i}
-          onClick={(event) => handleListItemClick( event, i )}
+          onClick={() => handleListItemClick( url, i )}
         >
-          <ListItemText primary={path} />
+          {/* <ListItemSecondaryAction>
+            <IconButton edge="start" aria-label="get-html">
+              <CodeIcon />
+            </IconButton>
+          </ListItemSecondaryAction> */}
+
+          <ListItemText primary={title} secondary={details} />
+
+          <ListItemSecondaryAction>
+            <IconButton edge="end" aria-label="get-url" onClick={() => copyUrl( url )}>
+              <LinkIcon />
+            </IconButton>
+          </ListItemSecondaryAction>
         </ListItem>
       ))}
       </List>
@@ -46,7 +72,7 @@ export const DesignBrowser = ( { debug } ) =>
   const report = useDispatch();
   const setUrl = url => report( fetchDesign( url, true ) );
 
-  const drawerColumns = 5;
+  const drawerColumns = 2;
   const canvasColumns = 12 - drawerColumns;
 
   return (
