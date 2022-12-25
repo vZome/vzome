0a2f7ef13aae55544db2a47976a2b5e52761045c
Scott Vorthmann <scott@vorthmann.org>
2022-12-24 21:38:25 -0800

    Fixed solid-react warning by switching to createRoot

--- online/src/app/classic/solid-react.jsx
+++ online/src/app/classic/solid-react.jsx
@@ -2,7 +2,7 @@
 // reusable wrapper to inject React inside Solid
 
 import { createEffect, untrack } from "solid-js";
-import { render } from "react-dom";
+import { createRoot } from 'react-dom/client';
 import { createElement } from "react";
 
 export const solidify = Component =>
@@ -10,9 +10,13 @@ export const solidify = Component =>
   return (props) => {
     let el;
     const result = <div ref={el} />;
+    const root = createRoot( el );
     createEffect(() => {
-      Object.values(props);
-      untrack( () => render( createElement( Component, props ), el ) );
+      Object.values( props );
+      untrack( () => {
+        root.render( createElement( Component, props ) );
+        // render( createElement( Component, props ), el );
+      } );
     });
     return result;
   };
