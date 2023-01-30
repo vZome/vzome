c6508650a6482b9a4197a6812de43c6f74a2059f
David Hall <David.42.Hall@gmail.com>
2023-01-30 16:32:09 -0500

    Fix high DPI scaling issues in Windows (and Linux?)
    * Remove JVM arg sun.java2d.uiScale=1.0.
    * Set system property "vzome.glcanvas.rescaling" based on OS at runtime.
    * Override GLCanvas if needed, as described at https://jogamp.org/bugzilla/show_bug.cgi?id=1374#c5
    * Rescale mouse position in pickRay() if needed

--- desktop/src/main/java/org/vorthmann/zome/render/jogl/JoglFactory.java
+++ desktop/src/main/java/org/vorthmann/zome/render/jogl/JoglFactory.java
@@ -1,6 +1,7 @@
 
 package org.vorthmann.zome.render.jogl;
 
+import java.awt.GraphicsConfiguration;
 import java.util.List;
 
 import org.vorthmann.j3d.J3dComponentFactory;
@@ -67,8 +68,46 @@ public class JoglFactory implements J3dComponentFactory
         // GLCanvas for the trackball view on the Mac gets "left behind" when resizing the window,
         //  a defect in JOGL.  The remedy is to use the lightweight GLJPanel for that situation,
         //  which behaves correctly, though it delivers a much lower frame rate than GLCanvas.
-        
-        GLAutoDrawable glcanvas = lightweight? new GLJPanel( glcapabilities, chooser ) : new GLCanvas( glcapabilities, chooser, null );
+        @SuppressWarnings("serial")
+		GLAutoDrawable glcanvas = lightweight
+        		? new GLJPanel( glcapabilities, chooser ) 
+        		: !"true".equals(System.getProperty("vzome.glcanvas.rescaling"))
+        			? new GLCanvas( glcapabilities, chooser, null ) // No override needed
+        			: new GLCanvas( glcapabilities, chooser, null ) {
+                	// Fix for High DPI scaling bug on Windows and Linux
+                	// Bug described at https://jogamp.org/bugzilla/show_bug.cgi?id=1358#c23
+            		// and https://forum.jogamp.org/JOGL-broken-with-JRE-8-and-Windows-window-scaling-tp4039122p4039665.html
+        			// 
+                	// Solution at https://jogamp.org/bugzilla/show_bug.cgi?id=1374#c5
+                	//
+                	// Sample code at the end of https://gist.github.com/maheshkurmi/e984430d33236b6bfb7c3de0f8a1a0e5
+                	// https://github.com/LWJGLX/lwjgl3-awt/issues/53#issuecomment-946005871
+                	// shows how to get the correct scaling factor at runtime even when switching monitors with different scaling factors.
+                	//
+                	// JoglRenderingViewer.pickRay() depends on these overridden methods.
+                	// They also make the model viewer display correctly on Windows High DPI monitors
+                	// with scaling set to anything other than 100%.
+                	//
+                	// This overload of getPreferredSize seems pointless but it's shown as part of the solution.
+                	// I'm just going to comment it out and see if it has any down side.
+//            		@Override
+//            		public Dimension getPreferredSize() {
+//            			Dimension d=super.getPreferredSize();
+//            			return new Dimension(d.width,d.height);
+//            		}
+            		@Override
+            		public int getWidth() {
+            			GraphicsConfiguration gc = getGraphicsConfiguration();
+            			double scale = gc == null ? 1.0 : gc.getDefaultTransform().getScaleX();
+            			return (int)(super.getWidth() * scale);
+            		}
+            		@Override
+            		public int getHeight() {
+            			GraphicsConfiguration gc = getGraphicsConfiguration();
+            			double scale = gc == null ? 1.0 : gc.getDefaultTransform().getScaleY();
+            			return (int)(super.getHeight() * scale);
+            		}
+            	};
         
         return new JoglRenderingViewer( scene, glcanvas );
     }
