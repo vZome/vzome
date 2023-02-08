855ef9881b97e9d79af0e9639d1a33db2e3b85c8
Scott Vorthmann <scott@vorthmann.org>
2023-02-07 20:45:33 -0800

    WIP experimenting

--- online/src/app/classic/editor.jsx
+++ online/src/app/classic/editor.jsx
@@ -1,15 +1,28 @@
 
 import React, { useRef } from 'react';
+import { TrackballControls } from '@react-three/drei';
+import { Vector3 } from 'three';
 
 import { SceneCanvas } from '../../ui/viewer/scenecanvas.jsx';
 
 // These are pure function React components, with no state, to better
 //  integrate with the SolidJS-based context.
 
+const pseudoCamera = {
+  position: new Vector3(),
+  up: new Vector3(),
+  lookAt: () => {},
+  isPerspectiveCamera: () => {
+    console.log( 'pseudoCamera isPerspectiveCamera' );
+    return true
+  },
+}
+
 const StrutDrag = ( { state } ) =>
 {
   return (
     <group position={ [0,0,-5] }>
+      <TrackballControls camera={pseudoCamera} staticMoving='true' rotateSpeed={4.5} zoomSpeed={3} panSpeed={1} />}
       <mesh>
         <meshLambertMaterial attach="material" color={"pink"} />
         <sphereBufferGeometry attach="geometry" args={[ 2 ]} />
