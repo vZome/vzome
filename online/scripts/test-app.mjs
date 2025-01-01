b2186e4e584267ca2a8e54a45b339d8e9d337744
Scott Vorthmann <scott@vorthmann.org>
2025-01-01 10:51:43 -0800

    Supporting quick workflow with inspector/debugger
    The legacy module (at r114) is dynamically imported from vzome.com.
    
    This lets me run the quick workflow in a devcontainer, and debug the inspector
    code.  I need this because I need to match the scene protocol used in this
    old branch, so I can make the old inspector client code work against
    modern worker code.

--- online/scripts/test-app.mjs
+++ online/scripts/test-app.mjs
@@ -11,19 +11,19 @@ const commonConfig = {
   outdir: 'public/modules',
 };
 
-const port = 8532;
+const port = 8855;
 
 if ( argv .includes( 'quick' ) ) {
   // Avoid bundling any of the legacy code; the 'vzome-legacy' dynamic bundle will be missing from runtime
   delete commonConfig.entryPoints[ 'vzome-legacy' ];
   commonConfig.external = [ './legacy/*.js' ];
 
-  writeFileSync( 'src/revision.js', 'export const REVISION="QUICKSTART";' );
+  writeFileSync( 'src/revision.js', 'export const REVISION="QUICKSTART"; export const resourceIndex = []; export const importLegacy = async () => import( "https://www.vzome.com/modules/r114/vzome-legacy.js" );' );
 
   console.log( '%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%' );
   console.log( '%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%' );
   console.log( '' );
-  console.log( `       visit  http://localhost:${port}/browser` );
+  console.log( `       visit  http://localhost:${port}/?debug=true` );
   console.log( '' );
   console.log( '%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%' );
   console.log( '%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%' );
