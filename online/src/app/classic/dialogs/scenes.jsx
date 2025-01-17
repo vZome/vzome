c59a986da6dedc77893a2cb26c2d4b7e85846485
Scott Vorthmann <scott@vorthmann.org>
2025-01-16 18:49:11 -0800

    Adding basic camera transfers to scenes dialog in online
    I'd like better icons, but the behavior is working perfectly.
    
    Still no Tooltip from SUID!

--- online/src/app/classic/dialogs/scenes.jsx
+++ online/src/app/classic/dialogs/scenes.jsx
@@ -1,12 +1,14 @@
 
-import { createSignal } from "solid-js"
 import { unwrap } from "solid-js/store"
 
 import DialogContent from "@suid/material/DialogContent"
 import Dialog from "@suid/material/Dialog"
 import DialogTitle from "@suid/material/DialogTitle"
 import DialogActions from "@suid/material/DialogActions"
-import Button from "@suid/material/Button"
+import Button from "@suid/material/Button";
+import IconButton from '@suid/material/IconButton';
+import CameraswitchIcon from '@suid/icons-material/Cameraswitch';
+import { Tooltip } from '../../framework/tooltip.jsx'
 
 import { useViewer } from "../../../viewer/context/viewer.jsx";
 import { useEditor } from '../../framework/context/editor.jsx';
@@ -15,7 +17,7 @@ import { CameraProvider } from "../../../viewer/index.jsx"
 import { SceneProvider } from "../../../viewer/context/scene.jsx"
 import { useCamera } from "../../../viewer/context/camera.jsx"
 
-const AddSceneButton = props =>
+const AddSceneButton = () =>
 {
   const { rootController, controllerAction, sceneIndex, setSceneIndex } = useEditor();
   const { state: { camera } } = useCamera();
@@ -54,7 +56,51 @@ const MoveSceneButton = props =>
     <Button variant="outlined" size="medium" onClick={moveScene} disabled={atLimit()}>{props.label}</Button>
   );
 }
+
+const UseCameraButton = props =>
+{
+  const { tweenCamera } = useCamera();
+  const { scenes } = useViewer();
+
+  const handleUseCamera = (evt,index) =>
+  {
+    evt.stopPropagation();
+    console.log( 'Using camera from scene', index );
+    tweenCamera( scenes[ index ] .camera );
+  }
   
+  return (
+    <Tooltip title='Use this camera' aria-label="camera-tip">
+      <IconButton color="primary" aria-label="camera" sx={{ position: 'absolute', bottom: '0px', right: '0px' }}
+          onClick={ (e) => handleUseCamera( e, props.index ) }>
+        <CameraswitchIcon fontSize="small"/>
+      </IconButton>
+    </Tooltip>
+  );
+}
+
+const SaveCameraButton = () =>
+{
+  const { rootController, controllerAction, sceneIndex } = useEditor();
+  const { state: { camera } } = useCamera();
+
+  const handleSaveCamera = (evt) =>
+  {
+    evt.stopPropagation();
+    const params = { index: sceneIndex(), camera: unwrap( camera ) };
+    controllerAction( rootController(), 'updateScene', params );
+  }
+    
+  return (
+    <Tooltip title='Save this camera to the scene' aria-label="camera-tip">
+      <IconButton color="primary" aria-label="camera" sx={{ position: 'absolute', top: '0px', left: '0px' }}
+          onClick={ (e) => handleSaveCamera( e ) }>
+        <CameraswitchIcon fontSize="large"/>
+      </IconButton>
+    </Tooltip>
+  );
+}
+    
 const ScenesDialog = props =>
 {
   const { scenes } = useViewer();
@@ -71,10 +117,11 @@ const ScenesDialog = props =>
                 <div class='scenes-list'>
                   <For each={ scenes } >{ (scene,i) =>
                     (i() > 0) &&
-                    <div class={ i()===sceneIndex()? 'scenes-entry scenes-selected' : 'scenes-entry' }
+                    <div class={ i()===sceneIndex()? 'scenes-entry scenes-selected' : 'scenes-entry' } style={{ position: 'relative' }}
                         onClick={ () => setSceneIndex( i() ) }>
                       <span>{i()}</span>
                       <span>{scene.title}</span>
+                      <UseCameraButton index={ i() } />
                     </div>
                   }</For>
                 </div>
@@ -90,6 +137,7 @@ const ScenesDialog = props =>
                 <SceneProvider name={ `#${sceneIndex()}` } config={{ preview: true, debug: false, labels: props.config?.labels, source: false }}>
                   <SceneCanvas height="100%" width="100%" />
                 </SceneProvider>
+                <SaveCameraButton/>
               </div>
             </div>
           </div>
