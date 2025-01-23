b237b04cb2d743a2078fd98c634c8602908b18e6
Scott Vorthmann <scott@vorthmann.org>
2025-01-22 21:50:19 -0800

    WIP Converting 4OFF (Stella4D) file to 4D mesh JSON
    I haven't nailed down the JSON format yet, so for now it is just an object in
    the console.  I should probably use the format that Nan and I agreed upon
    for the Observable notebook on Exploding 4D Polytopes.
    
    Note that there are no edges being computed yet; the OFF format never has edges.

---
+++ online/serve/app/test/cases/convert-4off/script.js
@@ -0,0 +1,50 @@
+
+const values = [
+  { real: 0.353553390593274, algebraic: [ 1, 0, 1 ] },
+  { real: 1.06066017177982,  algebraic: [ 3, 0, 1 ] },
+  { real: -0.353553390593274, algebraic: [ -1, 0, 1 ] },
+  { real: -1.06066017177982,  algebraic: [ -3, 0, 1 ] },
+]
+
+const realToAlgebraic = real => values .filter( value => Math.abs( value.real - real ) < 0.000000001 ) .pop() .algebraic;
+
+const toNumArray = line => line .split( ' ' ) .map( str => Number(str) );
+
+const interpret4OFF = lines =>
+{
+  // skip "4OFF" at lines[ 0 ]
+  const [ vertexCount, faceCount, edgeCount, cellCount ] = toNumArray( lines[ 1 ] );
+  let start = 2;
+  const vertices = [];
+  for ( var i = 0; i < vertexCount; i++ ) {
+    const data = toNumArray( lines[ i + start ] );
+    vertices .push( data .map( real => realToAlgebraic( real ) ) );
+  }
+  start += vertexCount;
+  const faces = [];
+  for ( var i = 0; i < faceCount; i++ ) {
+    const data = toNumArray( lines[ i + start ] );
+    const arity = data[ 0 ];
+    faces .push( data .slice( 1, arity+1 ) );
+  }
+  start += faceCount;
+  const cells = [];
+  for ( var i = 0; i < cellCount; i++ ) {
+    const data = toNumArray( lines[ i + start ] );
+    const arity = data[ 0 ];
+    cells .push( data .slice( 1, arity+1 ) );
+  }
+  console.dir( { vertices, faces, cells } );
+}
+
+document .getElementById( 'file' ) .onchange = function()
+{
+  var file = this.files[0];
+  var reader = new FileReader();
+  reader.onload = function()
+  {
+    var lines = this.result .split( /\r\n|\n/ ) .filter( line => line.trim() && ! line.startsWith( '#' ) );
+    interpret4OFF( lines );
+  };
+  reader .readAsText( file );
+};
