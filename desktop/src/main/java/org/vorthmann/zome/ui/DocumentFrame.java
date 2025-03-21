vZome/vzome

commit c7abccd7b8340b4e320a71215b359ae33bca8429

Update deprecated exec(String) to use exec(String[])

desktop/src/main/java/org/vorthmann/zome/ui/DocumentFrame.java
@@ -465,10 +465,23 @@ public class DocumentFrame extends JFrame implements PropertyChangeListener, Con
                             return;
                         }
                         String cmdLine = cmd .substring( "execCommandLine/" .length() );
+                        // convert the space delimited cmdLine to an array of Strings
+                        // in order to avoid using the deprecated overload of exec()
+                        String[] cmdArray = cmdLine.split(" "); // No special quote handling, just a simple split.
+                        for(int i = 0; i<cmdArray.length; i++) {
+                        	if(cmdArray[i] == "{}") {
+                        		cmdArray[i] = mFile.getName();
+                        	}
+                        }
                         cmdLine = cmdLine .replace( "{}", mFile .getName() );
                         logger.log( Level.INFO, "executing command line: " + cmdLine );
+                        if(cmdLine.contains("\"") || cmdLine.contains("'")) {
+                        	logger.log( Level.WARNING, "execCommandLine does no special handling of quotes characters.\n" 
+                        			+ " If the quotes characters in this command do not perform as expected,"
+                        			+ " consider invoking a simple shell script that accepts just the vZome file name as an argument." );
+                        }
                         try {
-                            Runtime .getRuntime() .exec( cmdLine, null, mFile .getParentFile() );
+                            Runtime .getRuntime() .exec( cmdArray, null, mFile .getParentFile() );
                         } catch ( IOException ioe ) {
                             System .err .println( "Runtime.exec() failed on " + cmdLine );
                             ioe .printStackTrace();

core/src/regression/java/com/vzome/core/regression/TestVZomeFiles.java
@@ -221,7 +221,8 @@ public class TestVZomeFiles extends FileSystemVisitor2 .Actor
                     } finally {
                         histOut .close();
                     }
-                    Process process = Runtime .getRuntime() .exec( "diff " + goldenHistory .getAbsolutePath() + " " + testHistory .getAbsolutePath() );
+                    String[] cmdArray = { "diff", goldenHistory .getAbsolutePath(), testHistory .getAbsolutePath() };
+                    Process process = Runtime .getRuntime() .exec( cmdArray );
                     // any error message?
                     StreamGobbler errorGobbler = new 
                             StreamGobbler( process.getErrorStream(), "ERROR" );            

core/src/test/java/quickhull3d/QuickHull3D.java
@@ -346,12 +346,13 @@ public class QuickHull3D {
     }
 
     protected void setFromQhull(double[] coords, int nump, boolean triangulate) {
-        String commandStr = "./qhull i";
+    	String[] cmdArray = { "./qhull", "i" };
         if (triangulate) {
-            commandStr += " -Qt";
+        	String[] cmdArray2 = { cmdArray[0], cmdArray[1], "-Qt" };
+        	cmdArray = cmdArray2;
         }
         try {
-            Process proc = Runtime.getRuntime().exec(commandStr);
+            Process proc = Runtime.getRuntime().exec(cmdArray);
             PrintStream ps = new PrintStream(proc.getOutputStream());
             StreamTokenizer stok = new StreamTokenizer(new InputStreamReader(proc.getInputStream()));
 

desktop/src/main/java/org/vorthmann/j3d/Platform.java
@@ -62,7 +62,8 @@ public class Platform
 		if ( isMac )
 			try {
 				String path = file .getAbsolutePath();
-				Runtime .getRuntime() .exec( "open " + path );
+				String[] cmdArray = { "open", path };
+				Runtime .getRuntime() .exec( cmdArray );
 			} catch ( IOException e ) {
 				System .err .println( "Runtime.exec() failed on " + file .getAbsolutePath() );
 				e .printStackTrace();

desktop/src/main/java/org/vorthmann/zome/ui/ApplicationUI.java
@@ -641,10 +641,10 @@ public final class ApplicationUI implements ApplicationController.UI, PropertyCh
     public void runScript( String script, File file )
     {
         try {
-            Runtime .getRuntime() .exec( script + " " + file .getAbsolutePath(),
-                    null, file .getParentFile() );
+        	String[] cmdArray = { script, file .getAbsolutePath() };
+			Runtime .getRuntime() .exec( cmdArray, null, file .getParentFile() );
         } catch ( IOException e ) {
-            System .err .println( "Runtime.exec() failed on " + file .getAbsolutePath() );
+            System .err .println( "Runtime.exec() failed on " + script + " " + file .getAbsolutePath() );
             e .printStackTrace();
         }
     }

commit 81f9cf1b6e99b24e4b4ce6c7167f618a09055fc4

Fix macro replacement bug in execCommandLine
Introduced in commit c7abccd7b8340b4e320a71215b359ae33bca8429

desktop/src/main/java/org/vorthmann/zome/ui/DocumentFrame.java
@@ -469,9 +469,7 @@ public class DocumentFrame extends JFrame implements PropertyChangeListener, Con
                         // in order to avoid using the deprecated overload of exec()
                         String[] cmdArray = cmdLine.split(" "); // No special quote handling, just a simple split.
                         for(int i = 0; i<cmdArray.length; i++) {
-                        	if(cmdArray[i] == "{}") {
-                        		cmdArray[i] = mFile.getName();
-                        	}
+                        	cmdArray[i] = cmdArray[i].replace( "{}", mFile .getName() );
                         }
                         cmdLine = cmdLine .replace( "{}", mFile .getName() );
                         logger.log( Level.INFO, "executing command line: " + cmdLine );