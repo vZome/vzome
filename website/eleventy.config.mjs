a3b8d17c94c2101598bc8e0a28e4915c23595a31
Scott Vorthmann <scott@vorthmann.org>
2025-11-19 10:03:35 -0800

    More template improvements for the website
    Date formats are right, as are post headers.
    
    I tuned a number of styles for the blog posts.
    
    I created a custom `geomPublished` collection to skip `published: false`
    blog posts, like Beautiful Jekyll does.
    
    The `website.yml` GitHub Action is once again targeted at `main`, in
    preparation for this branch to get merged.

--- website/eleventy.config.mjs
+++ website/eleventy.config.mjs
@@ -4,8 +4,14 @@ export default async function (eleventyConfig) {
 	eleventyConfig.addPassthroughCopy("assets");
 
   eleventyConfig.addGlobalData("layout", "page");
+  eleventyConfig.addGlobalData("page-type", "page");
 
 	// eleventyConfig.addLayoutAlias("vzome", "vzome-sharing/_layouts/vzome.html");
 
   eleventyConfig.addLiquidFilter( "relative_url", value => value );
+
+  eleventyConfig.addCollection( "geomPublished", (collectionsApi) => {
+		return collectionsApi .getFilteredByTag("posts") .filter( item => item.data.published !== false );
+  });
+
 };
